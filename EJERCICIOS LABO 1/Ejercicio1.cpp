a48218872bde47d409222504a537e22321e00171
edwinma3046 <00013420@uca.edu.sv>
2020-05-06 17:31:28 -0600

    Labo1

---
+++ EJERCICIOS LABO 1/Ejercicio1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+
+    int numero1;
+    int numero2;
+    int numero3;
+    float promedio;
+
+    cout << endl << "Por favor ingrese su primer numero entero: " << endl;
+    cin >> numero1;
+   
+    cout << endl << "Por favor ingrese su segundo numero entero: " << endl;
+    cin >> numero2;
+   
+    cout << endl << "Por favor ingrese su tercer numero entero: " << endl;
+    cin >> numero3;
+
+    promedio = (numero1+numero2+numero3)/3;
+    cout << endl << "Su promedio es de: " << endl << promedio;
+
+   
+}
