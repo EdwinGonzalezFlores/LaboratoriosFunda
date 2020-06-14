313c273a8660a3ea7758a00c3c8a5b707c93ecf5
edwinma3046 <00013420@uca.edu.sv>
2020-06-13 21:22:50 -0600

    Ejer2

--- Tercer Corto/Ejercicio2.cpp
+++ Tercer Corto/Ejercicio2.cpp
@@ -1,10 +1,3 @@
-/*Se desea un programa en el cual el usuario pueda jugar adivinando un número que está
-previamente guardo en el programa.
-El número debe de estar en el rango de 1 a 100, solo debe de trabajar con números enteros.
-El usuario tendrá 5 intentos para poder adivinar el número. Por cada intento el programa  deberá 
-darle estas pistas al usuario: si el número que ingresa  es menor al número secreto,   si el número 
-que ingresa mayor al número que está intentando adivinar y cuantos intentos le quedan.  
-Si el usuario ya no quiere jugar, indicarle qué pulsar para  finalizar el programa.*/
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
@@ -13,140 +6,49 @@ using namespace std;
 int num= 1+rand()%(101-1), numerox, intentos = 5;
 char Tecla;
 
-string probar(int h){
-    string a, b, c;
-    int num= 1+rand()%(101-1);
-    
-    a = "Te pasaste. Elige un numero menor: ";
-    b = "Muy bajo! Elige un numero mayor para seguir: ";
-    c = "FELICIDADES!! Has adivinado el numero!";
-    
-    
-    if (h > num){
-        return a;
-    }else if (h < num){
-        return b;
-    }else{
-        return c;
-    }
-   
-}
-
-string seguir(char y){
-    switch (y)
-    {
-    case 'E': cout << endl << "Gracias por participar";
-        break;
-    
-    default: cout << "Te quedan "<< intentos << " intentos. ";
-        break;
-    }
-    return 0;
-}
 int main(void){
 
-    cout << endl << "NÚMERO MAGICO" << endl;
-    do
-    {
-        
-        cout << endl << "Ingresa el numero que piensas es el correcto:" << endl;
-        cin >> numerox;
-        cout << endl;
-
-        --intentos;
-
-        probar(numerox);
-
-        cout << "Quieres seguir jugando?" << endl << "Presiona la letra 'E' MAYUSCULA para salir del juego.";
-        cout << endl << "De lo contrario presiona otra tecla cualquiera: ";
-        cin >> Tecla; cout << endl;
-
-        seguir(Tecla);
-    
-
-
-    } while (intentos > 0 || numerox != num);
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-int comprobar(int x){
-     
-    while (x > 0 && x < 100)
-    {
-        
-        if (x > num)
-        {
-            cout << endl << "El numero " << x << " es mayor que el numero a adivinar." << endl;
-        }
-        if (x < num)
-        {
-            cout << endl << "El numero " << x << " es menor que el numero a adivinar" << endl; 
-        }
-        if (x = num)
-        {
-            cout << "Felicidades, Has adivinado el numero!" << "Este es " << num << endl;
-        }
-    }
-    cout << endl << "No has introducido un número válido.";
-    cin >> x;
-    cout << endl;
-    return 0;
-}
-
-
-string seguir(char y){
-
-    switch (y)
-    {
-    case 'E': case 'e': cout << endl << "Gracias por participar";
-        break;
-    
-    default: cout << "Te quedan "<< intentos << " intentos. ";
-        break;
-    }
-    return 0;
-}
-
-
-int main(void){
-    cout << num;
-    
-    *//*cout << "NUMERO MAGICO" << endl << endl;
+    cout << endl << "ADIVINA UN NUMERO" << endl;
 
     do
     {
-        cout << endl << "Por favor introduce un numero: ";
+        cout<< "Ingresa un numero y comprueba si has ganado: " << endl;
         cin >> numerox;
-        cout << endl;
-        --intentos;
-        cout << comprobar(numerox);
-        cout << "Quieres seguir jugando?";
-        cin >> Tecla;
-        cout << seguir(Tecla);
-    } while ((intentos > 0) || numerox == num);
-    
+        intentos--;
+        if (numerox > num)
+        {
+            cout << "Menor" << endl << "Quieres seguir jugando? Presiona 'E' en MAYUSCULAS si quieres salir" << endl;
+            cin >> Tecla;
+            switch (Tecla)
+            {
+            case 'E':
+                cout << "Gracias por participar." << endl;
+                break;
+            
+            default: cout << "Te quedan " << intentos << " intentos." << endl;
+                break;
+            }
+        }else if (numerox < num)
+        {
+            cout << "Mayor" << endl << "Quieres seguir jugando? Presiona 'E' en MAYUSCULAS si quieres salir" << endl;
+            cin >> Tecla;
+            switch (Tecla)
+            {
+            case 'E':
+                cout << "Gracias por participar." << endl;
+                break;
+            
+            default: cout << "Te quedan " << intentos << " intentos." << endl;
+                break;
+            };
+        }else
+        {
+            cout << "Felicidades.";
+        }
+        
+            
+
+    } while (intentos > 0 && numerox != num && Tecla != 'E');
     
     return 0;
-}*/
+}
