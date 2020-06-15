f92bde5448144bf4bd7c9b0068649f5f600b1787
edwinma3046 <00013420@uca.edu.sv>
2020-06-15 10:34:19 -0600

    Labo5

---
+++ EJERCICIO LABO 5/Ejercicio1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int mcd(int x, int y){
+    int mcd;
+    
+    mcd = (x%y);
+
+    if (mcd == 0)
+    {
+        cout << "El MCD de los numeros " << x << " y " << y << " es ";
+        return y;
+    }
+    
+    while (mcd != 0)
+    {
+        x = y;
+        y = mcd;
+        mcd = (x%y);
+
+        if (mcd == 0)
+        {
+            cout << "El MCD de ambos numeros es ";
+            return y;
+        }
+        
+    }
+    
+   
+}
+
+
+int main(void){
+
+    int dividendo;
+    int divisor;
+    
+
+    cout << endl << endl << "CALCULAR MCD" << endl;
+    cout << "Por favor introduce el numero mayor (Dividendo): ";
+    cin >> dividendo;
+    cout << endl << "Por favor intrdouce el numero menor (Divisor): ";
+    cin >> divisor;
+
+    cout << mcd(dividendo, divisor);
+
+
+    return 0;
+}
---
+++ EJERCICIO LABO 5/Ejercicio2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int hora, min, seg;
+
+
+    cout << endl<< "HORAS" << endl;
+    cout << "Por favor introduzca la hora (En formato de 24 horas): ";
+    cin >> hora;
+
+    cout << "Minutos: ";
+    cin >> min;
+
+    cout << "Segundos: ";
+    cin >> seg;
+
+    if (hora >= 0 && hora <= 23 && min >= 0 && min <= 59 && seg >= 0 && seg <= 59)
+    {
+        ++seg;
+        if (seg == 60)
+        {
+            seg = 00;
+            min++;
+            if (min == 60)
+            {
+                min = 00;
+                hora++;
+                if (hora == 24)
+                {
+                    hora = 00;
+                }
+            }    
+        }
+        cout << "La hora un segundo despues es: " << hora << ":" << min << ":" << seg;
+    }
+}
