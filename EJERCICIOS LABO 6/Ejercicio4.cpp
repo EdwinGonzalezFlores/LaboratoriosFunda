5412f2c153845eb5b72510c5cff81afaa894b999
edwinma3046 <00013420@uca.edu.sv>
2020-06-23 09:56:13 -0600

    sisoi2

--- EJERCICIOS LABO 6/Ejercicio4.cpp
+++ EJERCICIOS LABO 6/Ejercicio4.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 using namespace std;
+
+/*En esta función "arreglo" se le pide al usuario ingresar los datos de los dos arreglos, más adelante en la función principal, para luego
+mostrarlos en pantalla.*/
 void arreglo(int z[], int n, string x){
    
 
@@ -19,12 +22,14 @@ void arreglo(int z[], int n, string x){
     
 }
 
+/*En esta función "suma" se piden dos arreglos (a y b) los cuales son sumados y dan como resultado el arreglo 'suma'. Simplemente se realiza la suma pero no se muestra nada.*/
 void suma(int a[], int b[], int n, int suma[]){
     for(int i = 0;i < n;i++){
     suma[i]=a[i] + b[i];
     }
 }
 
+/*En esta función se pide el arreglo anterior 'suma' y se muestra por pantalla.*/
 int resultado(int a[], int n){
     for(int i = 0;i < n;i++){
     cout << a[i] << ' ';
@@ -41,22 +46,22 @@ int main(void){
     b = "arreglo 2";
     cout << endl << endl << "SUMA DE ARREGLOS" << endl;
     
-    cout << endl << "Ingrese el tama" << (char)164 <<"o del arreglo 1: ";
+    cout << endl << "Ingrese el tama" << (char)164 <<"o del arreglo 1: ";       //Se pide el tamaño para ambos arreglos
     cin >> n;
 
     int array1[n];
-    arreglo(array1, n, a);
+    arreglo(array1, n, a);      //Se aplica la función arreglo (Creada arriba) al primer arreglo
 
     cout << endl << endl << "Ahora, por favor ingresa la misma cantidad de datos para el " << b;
     int array2[n];
-    arreglo(array2, n, b);
+    arreglo(array2, n, b);      //Se realiza lo mismo para el arreglo numero 2
 
-    int arreglosuma[n];
-    suma(array1, array2, n, arreglosuma);
+    int arreglosuma[n];     //Se crea un arreglo donde se guardará la suma
+    suma(array1, array2, n, arreglosuma);       //Se aplcia la función suma definida arriba
 
     cout << endl << endl << "La suma de los dos arreglos dara como resultado: " << endl << "[ ";
 
-    resultado(arreglosuma, n);
+    resultado(arreglosuma, n);      //Se muestra el resultado de la suma
 
 
     
--- EJERCICIOS LABO 6/Ejercicio6.cpp
+++ EJERCICIOS LABO 6/Ejercicio6.cpp
@@ -7,7 +7,7 @@ int main(void){
     int n, numero, contador = 0;
 
     cout << endl << endl << "ENCONTRAR ENTEROS" << endl;
-    
+    //Se pide el tamaño del arreglo
     cout << endl << "Ingrese el tama" << (char)164 <<"o del arreglo: ";
     cin >> n;
 
@@ -15,33 +15,30 @@ int main(void){
 
    for (int i = 0; i < n; i++)
     {
-        cout << endl << "Ingrese los datos del arreglo: ";
+        cout << endl << "Ingrese los datos del arreglo: ";      //Se ingresan los datos del arreglo
         cin >> arreglo[i];
     }
 
-    cout << "El arreglo contiene: [ ";
+    cout << "El arreglo contiene: [ ";      //Se muestran los datos del arreglo
     for(int i = 0; i < n; i++){
          cout << arreglo[i] << ' ' ;
     }
     cout << "]";
 
-    cout << endl << endl << "Ingrese el numero a buscar en el arreglo: ";
+    cout << endl << endl << "Ingrese el numero a buscar en el arreglo: ";       //Se pide el número a buscar dentro del arreglo
     cin >> numero;
 
     for (int i = 0; i < n; i++)
     {
-        if (arreglo[i] == numero)
+        if (arreglo[i] == numero)       //Se busca dentro del arreglo los números que sean igual al número ingresado por el usuario. Luego, por cada vez que sean iguales
+                                        //Se aumenta en uno el contador
         {
             contador++;
         }
         
     }
 
-
-    cout << endl << "El numero de veces que " << numero << " aparece en el arreglo es " << contador << " veces";
-
-
-
+    cout << endl << "El numero de veces que " << numero << " aparece en el arreglo es " << contador << " veces";        //Se muestra las veces que aparece el numero en el arreglo
 
     return 0;
 }
