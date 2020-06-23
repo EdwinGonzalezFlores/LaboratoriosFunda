3d934beaab97992d39e72a3b5b450a525d5d6697
edwinma3046 <00013420@uca.edu.sv>
2020-06-22 22:27:42 -0600

    sisoi

--- EJERCICIOS LABO 6/Ejercicio4.cpp
+++ EJERCICIOS LABO 6/Ejercicio4.cpp
@@ -1,41 +1,65 @@
 #include <iostream>
 
 using namespace std;
-
-int arreglo(int z[], int n){
+void arreglo(int z[], int n, string x){
+   
 
     for (int i = 0; i < n; i++)
     {
-        cout << "Ingrese los datos de : ";
+        cout << endl << "Ingrese los datos del arreglo: ";
         cin >> z[i];
     }
 
-    cout << "El  contiene a : [";
+    cout << "El " << x << " contiene: [ ";
     for(int i = 0; i < n; i++){
-        cout << z[i] << " ";
+         cout << z[i] << ' ' ;
     }
     cout << "]";
     
-    return 0;
+}
+
+void suma(int a[], int b[], int n, int suma[]){
+    for(int i = 0;i < n;i++){
+    suma[i]=a[i] + b[i];
+    }
+}
+
+int resultado(int a[], int n){
+    for(int i = 0;i < n;i++){
+    cout << a[i] << ' ';
+    }
+    cout <<  "]";
+
 }
 
 int main(void){
 
-    int m, n;
-    int array1[m], array2[n];
+    int n;
     string a, b;
-    a = "Arreglo 1";
-    b = "Arreglo 2";
-
-    cout << endl << "SUMA DE ARREGLOS" << endl;
+    a = "arreglo 1";
+    b = "arreglo 2";
+    cout << endl << endl << "SUMA DE ARREGLOS" << endl;
     
     cout << endl << "Ingrese el tama" << (char)164 <<"o del arreglo 1: ";
-    cin >> m;
-    cout << arreglo(array1, m);
-
-    cout << endl << "Ingrese el tama" << (char)164 <<"o del arreglo 2: ";
     cin >> n;
-    cout << arreglo(array2, n);
+
+    int array1[n];
+    arreglo(array1, n, a);
+
+    cout << endl << endl << "Ahora, por favor ingresa la misma cantidad de datos para el " << b;
+    int array2[n];
+    arreglo(array2, n, b);
+
+    int arreglosuma[n];
+    suma(array1, array2, n, arreglosuma);
+
+    cout << endl << endl << "La suma de los dos arreglos dara como resultado: " << endl << "[ ";
+
+    resultado(arreglosuma, n);
+
+
     
+
     return 0;
 }
+
---
+++ EJERCICIOS LABO 6/Ejercicio6.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+    int n, numero, contador = 0;
+
+    cout << endl << endl << "ENCONTRAR ENTEROS" << endl;
+    
+    cout << endl << "Ingrese el tama" << (char)164 <<"o del arreglo: ";
+    cin >> n;
+
+    int arreglo[n];
+
+   for (int i = 0; i < n; i++)
+    {
+        cout << endl << "Ingrese los datos del arreglo: ";
+        cin >> arreglo[i];
+    }
+
+    cout << "El arreglo contiene: [ ";
+    for(int i = 0; i < n; i++){
+         cout << arreglo[i] << ' ' ;
+    }
+    cout << "]";
+
+    cout << endl << endl << "Ingrese el numero a buscar en el arreglo: ";
+    cin >> numero;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arreglo[i] == numero)
+        {
+            contador++;
+        }
+        
+    }
+
+    cout << endl << "El numero de veces que " << numero << " aparece en el arreglo es " << contador << " veces";
+
+
+
+
+    return 0;
+}
